9eb8fd6383a80592b384f66acf39bbe5a7139820
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-03-22 06:52:12 +1100

    make it tail access log, comment

--- runsquid.sh
+++ runsquid.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "Getting IP"
+# Setup the NAT rule that enables transparent proxying
 IPADDR=$(/sbin/ifconfig eth0 | grep 'inet addr' | awk 'BEGIN { FS = "[ :]+" } ; { print $4 }')
-echo "IP Address is ${IPADDR}, setting up DNAT"
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${IPADDR}:3128
 
-echo "Chown"
+# Make sure our cache is setup
 chown proxy.proxy /var/spool/squid3
-echo "Building cache directories"
-squid3 -z 2>/dev/null
-echo "Executing Squid"
-squid3 -N -d 1
-echo "Completed"
+[ -e /var/spool/squid3/swap.state ] || squid3 -z 2>/dev/null
+
+# Run squid and tail the logs
+squid3
+while true; do
+  [ -e /var/log/squid3/access.log ] && tail -f /var/log/squid3/access.log
+  sleep 1
+done
