f17507664e0712b423d2972a4df2f9b5aaf061c1
Filip Krikava <krikava@gmail.com>
2014-05-19 15:00:45 +0200

    Use sh instead of bash so it is compatible with tiny core linux

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Script to maintain ip rules on the host when starting up a transparent
 # proxy server for docker.
@@ -19,7 +19,7 @@ set -e
 start_routing () {
   # Add a new route table that routes everything marked through the new container
   grep TRANSPROXY /etc/iproute2/rt_tables >/dev/null || \
-    sudo bash -c "echo '1	TRANSPROXY' >> /etc/iproute2/rt_tables"
+    sudo sh -c "echo '1	TRANSPROXY' >> /etc/iproute2/rt_tables"
   ip rule show | grep TRANSPROXY >/dev/null || \
     sudo ip rule add from all fwmark 0x1 lookup TRANSPROXY
   sudo ip route add default via ${IPADDR} dev docker0 table TRANSPROXY
