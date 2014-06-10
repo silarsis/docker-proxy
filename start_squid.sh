021422b7e2f63282adde8714e0a316d4dab2578c
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-06-11 07:02:22 +1000

    some small changes to match newer boot2docker

--- start_squid.sh
+++ start_squid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup the NAT rule that enables transparent proxying
-IPADDR=$(/sbin/ifconfig eth0 | grep 'inet addr' | awk 'BEGIN { FS = "[ :]+" } ; { print $4 }')
+IPADDR=$(/sbin/ip -o -f inet addr show eth0 | awk '{ sub(/\/.+/,"",$4); print $4 }')
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${IPADDR}:3128
 
 # Make sure our cache is setup
